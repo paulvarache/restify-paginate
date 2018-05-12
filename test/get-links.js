f1612fc41438a9d4b187eda6223a7740915f02a4
Amandio Magalhaes <amandio.magalhaes@gmail.com>
2018-05-12 15:46:52 -0700

    Fixed tests

--- test/get-links.js
+++ test/get-links.js
@@ -6,6 +6,10 @@ var restify = require('restify'),
     server;
 
 describe('The getLinks() function', function () {
+    const baseUrl = 'http://localhost';
+    const basePort = 8888;
+    const baseUri = `${baseUrl}:${basePort}/`;
+
     before(function (done) {
         server = restify.createServer({
             name: 'test'
@@ -17,7 +21,7 @@ describe('The getLinks() function', function () {
 
         server.get('/test', function (req, res, next) {
             res.send({
-            	pages: res.paginate.getLinks(10)
+                pages: res.paginate.getLinks(10)
             });
         });
         server.get('/test-no-count', function (req, res, next) {
@@ -26,18 +30,29 @@ describe('The getLinks() function', function () {
             });
         });
 
-        server.listen(8888, done);
+        server.listen(basePort, done);
     });
 
-    it('should generate a prev, next, first and last page link, if count is provided', function(done) {
+    it('should generate a prev, next, first and last page link, if count is provided', function (done) {
         request({
-            uri: 'http://localhost:8888/test?page=3&per_page=2',
+            baseUrl: baseUri,
+            uri: 'test?page=3&per_page=2',
             json: true
         }).then(function (res) {
-            url.parse(res.pages.prev, true).query.should.have.property('page', '2');
-            url.parse(res.pages.next, true).query.should.have.property('page', '4');
-            url.parse(res.pages.first, true).query.should.have.property('page', '1');
-            url.parse(res.pages.last, true).query.should.have.property('page', '5');
+            should.exist(res.pages);
+
+            const prevUrl = Object.assign({}, url.parse(res.pages.prev, true).query);
+            prevUrl.should.have.property('page', '2');
+
+            const nextUrl = Object.assign({}, url.parse(res.pages.next, true).query);
+            nextUrl.should.have.property('page', '4');
+
+            const firstUrl = Object.assign({}, url.parse(res.pages.first, true).query);
+            firstUrl.should.have.property('page', '1');
+
+            const lastUrl = Object.assign({}, url.parse(res.pages.last, true).query);
+            lastUrl.should.have.property('page', '5');
+
             done();
         }, function (err) {
             console.log(err);
@@ -45,14 +60,21 @@ describe('The getLinks() function', function () {
         });
     });
 
-    it('should generate a prev, next and first (but not last) page link, if count isn\'t provided', function(done) {
+    it('should generate a prev, next and first (but not last) page link, if count isn\'t provided', function (done) {
         request({
-            uri: 'http://localhost:8888/test-no-count?testparam=test&page=3&per_page=2',
+            baseUrl: baseUri,
+            uri: 'test-no-count?testparam=test&page=3&per_page=2',
             json: true
         }).then(function (res) {
-            url.parse(res.pages.prev, true).query.should.have.property('page', '2');
-            url.parse(res.pages.next, true).query.should.have.property('page', '4');
-            url.parse(res.pages.first, true).query.should.have.property('page', '1');
+            const prevUrl = Object.assign({}, url.parse(res.pages.prev, true).query);
+            prevUrl.should.have.property('page', '2');
+
+            const nextUrl = Object.assign({}, url.parse(res.pages.next, true).query);
+            nextUrl.should.have.property('page', '4');
+
+            const firstUrl = Object.assign({}, url.parse(res.pages.first, true).query);
+            firstUrl.should.have.property('page', '1');
+
             res.pages.should.not.have.property('last');
             done();
         }, function (err) {
@@ -61,12 +83,13 @@ describe('The getLinks() function', function () {
         });
     });
 
-    it('should add the params of the initial request to the generated links, except for page', function(done) {
-    	request({
-            uri: 'http://localhost:8888/test?testparam=test&page=2&per_page=2',
+    it('should add the params of the initial request to the generated links, except for page', function (done) {
+        request({
+            baseUrl: baseUri,
+            uri: 'test?testparam=test&page=2&per_page=2',
             json: true
         }).then(function (res) {
-            var nextLinkParams = url.parse(res.pages.next, true).query;
+            var nextLinkParams = Object.assign({}, url.parse(res.pages.next, true).query);
             nextLinkParams.should.have.property('testparam', 'test');
             nextLinkParams.should.have.property('per_page', '2');
             nextLinkParams.should.not.have.property('page', '2');
@@ -77,16 +100,17 @@ describe('The getLinks() function', function () {
         });
     });
 
-    it('shouldn\'t return an empty last page, in case count % per_page === 0', function(done) {
+    it('shouldn\'t return an empty last page, in case count % per_page === 0', function (done) {
         request({
-            uri: 'http://localhost:8888/test?page=2&per_page=2',
+            baseUrl: baseUri,
+            uri: 'test?page=2&per_page=2',
             json: true
         }).then(function (res) {
-            var lastLinkParams = url.parse(res.pages.last, true).query;
+            var lastLinkParams = Object.assign({}, url.parse(res.pages.last, true).query);
 
             // the count is 10 and per_page is 2, therefore the last page should be 5
             lastLinkParams.should.have.property('page', '5');
-            
+
             done();
         }, function (err) {
             console.log(err);
